28b13bfcb55fab796d64d6d1cc4b32d35016f95b
Dmytro <areality.dt@gmail.com>
2023-10-26 12:32:27 +0300

    Methods clear(),get(), size() updated. Variables naming enhanced

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,45 +3,48 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        while (size() != 0) {
-            delete(storage[0].uuid);
+        if (size > 0) {
+          for (int i = 0; i < size; i++) {
+              storage[i] = null;
+          }
+          size = 0;
         }
     }
 
+
     void save(Resume r) {
-        int size = size();
         if (size < storage.length) {
             storage[size] = r;
+            size++;
         } else {
             System.out.println("Ошибка. Хранилище заполнено!");
         }
     }
 
     Resume get(String uuid) {
-        int left = 0, right = size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int res = uuid.compareTo(storage[mid].uuid);
-            if (res == 0) return storage[mid];
-            else if (res > 0) left = mid + 1;
-            else right = mid - 1;
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            int res = uuid.compareTo(storage[i].uuid);
+            if (res == 0) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        if (size() > 0) {
+        if (size > 0) {
             int index = -1;
-            for (int i = 0; i < size(); i++) {
+            for (int i = 0; i < size; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     index = i;
                     break;
                 }
             }
             if (index >= 0) {
-                System.arraycopy(storage, index + 1, storage, index, size() - index);
+                System.arraycopy(storage, index + 1, storage, index, size - index);
+                size--;
             }
         }
     }
@@ -50,18 +53,13 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] res = new Resume[size()];
-        System.arraycopy(storage, 0, res, 0, size());
-        return res;
+        int size = size();
+        Resume[] allResumes = new Resume[size];
+        System.arraycopy(storage, 0, allResumes, 0, size);
+        return allResumes;
     }
 
     int size() {
-        int size = 0;
-        for (Resume i : storage) {
-            if (i != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
