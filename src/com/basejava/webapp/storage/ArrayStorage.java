607867769601e03b7646d4b3df6d5258cbecbee5
Dmytro <areality.dt@gmail.com>
2023-10-30 14:06:37 +0300

    method update() enhanced and added to MainTestArrayStorage class

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -1,6 +1,7 @@
 package com.basejava.webapp.storage;
 
 import com.basejava.webapp.model.Resume;
+import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
@@ -11,24 +12,29 @@ public class ArrayStorage {
 
     public void clear() {
         if (size > 0) {
-          for (int i = 0; i < size; i++) {
-              storage[i] = null;
-          }
-          size = 0;
+            Arrays.fill(storage, 0, size, null);
+            size = 0;
+        } else {
+            System.out.println("Ошибка. База данных пустая");
         }
     }
 
     public void update(Resume r) {
-
+        int i = findIndex(r.getUuid());
+        if (i != -1) {
+            storage[i] = r;
+            System.out.println("Резюме " + r.getUuid() + " успешно обновлено");
+        } else {
+            System.out.println("Ошибка. Резюме " + r.getUuid() + " не найдено. Попробуйте еще раз");
+        }
     }
+
     public void save(Resume r) {
-        // TODO check if resume is not present
-        // size < storage.length
         if (findIndex(r.getUuid()) == -1) {
             storage[size] = r;
             size++;
             System.out.println("Резюме " + r.getUuid() + " успешно добавлено");
-        } else if (size < storage.length){
+        } else if (size > storage.length){
             System.out.println("Ошибка. База данных заполнена!");
         } else {
             System.out.println("Ошибка. Резюме " + r.getUuid() + " уже находится в базе данных");
@@ -39,6 +45,7 @@ public class ArrayStorage {
         int i = findIndex(uuid);
         if (i == -1) {
             System.out.println("Резюме " + uuid + " не найдено. Попробуйте еще раз!");
+            return null;
         }
         return storage[i];
     }
@@ -50,11 +57,12 @@ public class ArrayStorage {
         }
         else if (size == 0) {
             System.out.println("База данных пустая, удаление невозможно");
-        }
+        } else {
         System.arraycopy(storage, i + 1, storage, i, size - 1);
         size--;
         System.out.println("Резюме " + uuid + " успешно удалено");
         }
+    }
 
     private int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
@@ -67,9 +75,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] allResumes = new Resume[size];
-        System.arraycopy(storage, 0, allResumes, 0, size);
-        return allResumes;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
--- src/com/basejava/webapp/storage/MainArray.java
+++ src/com/basejava/webapp/storage/MainArray.java
@@ -16,7 +16,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save uuid | update | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -38,6 +38,10 @@ public class MainArray {
                     r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
+                    break;
+                case "update":
+                    r = new Resume();
+                    ARRAY_STORAGE.update(r);
                     break;
                 case "delete":
                     ARRAY_STORAGE.delete(uuid);
