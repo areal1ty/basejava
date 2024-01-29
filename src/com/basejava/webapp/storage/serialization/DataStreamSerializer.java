e6397356dd92e17d36b6deb1e723870fa4ea0b35
Dmytro <areality.dt@gmail.com>
2024-01-29 19:27:49 +0300

    DataStreamSerializer refactored and tested

--- src/com/basejava/webapp/storage/serialization/DataStreamSerializer.java
+++ src/com/basejava/webapp/storage/serialization/DataStreamSerializer.java
@@ -13,11 +13,15 @@ public class DataStreamSerializer implements StreamSerializeStrategy{
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume r = new Resume(uuid, fullName);
-            readComponents(() -> r.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()), dis);
-            readComponents(() -> {
+            readComponents(dis, () -> {
+                    ContactType contactType = ContactType.valueOf(dis.readUTF());
+                    String contactValue = dis.readUTF();
+                    r.addContact(contactType, contactValue);
+                });
+            readComponents(dis, () -> {
                 SectionType sT = SectionType.valueOf(dis.readUTF());
                 r.addSection(sT, readSection(sT, dis));
-            }, dis);
+            });
             return r;
         }
     }
@@ -27,40 +31,42 @@ public class DataStreamSerializer implements StreamSerializeStrategy{
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
-            for (Map.Entry<ContactType, String> entry : r.getContacts().entrySet()) {
+
+            writeInCollection(r.getContacts().entrySet(), dos, entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
-            writeInCollection(r.getSections().entrySet(), entry -> {
+            });
+
+            writeInCollection(r.getSections().entrySet(), dos, entry -> {
                 SectionType t = entry.getKey();
                 Section s = entry.getValue();
                 dos.writeUTF(t.name());
                 switch (t) {
-                    case PERSONAL, OBJECTIVE, ACHIEVEMENTS, QUALIFICATIONS -> writeInCollection(((ListSection) s).getItems(), dos::writeUTF, dos);
-                    case EXPERIENCE, EDUCATION -> writeInCollection(((OrganizationSection) s).getOrganization(), organization -> {
+                    case PERSONAL, OBJECTIVE, ACHIEVEMENTS, QUALIFICATIONS -> writeInCollection(((ListSection) s).getItems(), dos, dos::writeUTF);
+                    case EXPERIENCE, EDUCATION -> writeInCollection(((OrganizationSection) s).getOrganization(), dos, organization -> {
                         dos.writeUTF(organization.getWebsite().getTitle());
                         dos.writeUTF(organization.getWebsite().getUrl());
-                        writeInCollection(organization.getPeriods(), period -> {
+                        writeInCollection(organization.getPeriods(), dos, period -> {
                             writeLD(period.getDateOfStart(), dos);
                             writeLD(period.getDateOfEnd(), dos);
                             dos.writeUTF(period.getTitle());
                             dos.writeUTF(period.getDescription());
-                        }, dos);
-                    }, dos);
+                        });
+                    });
                 }
-            }, dos);
+            });
         }
     }
 
     private Section readSection(SectionType sectionType, DataInputStream dis) throws IOException{
         return switch (sectionType) {
-            case PERSONAL, OBJECTIVE, ACHIEVEMENTS, QUALIFICATIONS -> new ListSection(readArrayList(dis::readUTF, dis));
+            case PERSONAL, OBJECTIVE, ACHIEVEMENTS, QUALIFICATIONS -> new ListSection(readInArrayList(dis, dis::readUTF));
             case EXPERIENCE, EDUCATION -> new OrganizationSection(
-                    readArrayList(() -> new Organization(
+                    readInArrayList(dis, () -> new Organization(
                             dis.readUTF(), dis.readUTF(),
-                            readArrayList(() -> new Period(
+                            readInArrayList(dis, () -> new Period(
                                     readLD(dis), readLD(dis),
-                                    dis.readUTF(), dis.readUTF()), dis)), dis));
+                                    dis.readUTF(), dis.readUTF())))));
         };
     }
 
@@ -73,7 +79,7 @@ public class DataStreamSerializer implements StreamSerializeStrategy{
         dos.writeInt(ld.getMonthValue());
     }
 
-    private <T> List<T> readArrayList(ComponentReader<T> componentReader, DataInputStream dis) throws IOException{
+    private <T> List<T> readInArrayList(DataInputStream dis, ComponentReader<T> componentReader) throws IOException{
         int size = dis.readInt();
         List<T> l = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
@@ -82,15 +88,14 @@ public class DataStreamSerializer implements StreamSerializeStrategy{
         return l;
     }
 
-    private void readComponents(ComponentProcessor p, DataInputStream dis) throws IOException{
+    private void readComponents(DataInputStream dis, ComponentProcessor p) throws IOException{
         int size = dis.readInt();
-        if (dis.available() >= 4) {
         for (int i = 0; i < size; i++) {
             p.process();
-        }}
+        }
     }
 
-    private <T> void writeInCollection(Collection<T> collection, ComponentWriter<T> componentWriter, DataOutputStream dos) throws IOException{
+    private <T> void writeInCollection(Collection<T> collection, DataOutputStream dos, ComponentWriter<T> componentWriter) throws IOException{
         dos.writeInt(collection.size());
         for (T component : collection) {
             componentWriter.write(component);
