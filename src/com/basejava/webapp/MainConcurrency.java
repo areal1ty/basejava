69628e7d86f4a86d1ca53b22c322b69e983a943a
Dmytro <areality.dt@gmail.com>
2024-02-01 23:33:35 +0300

    minValue() and oddOrEven() methods implemented

--- src/com/basejava/webapp/MainConcurrency.java
+++ src/com/basejava/webapp/MainConcurrency.java
@@ -1,43 +1,73 @@
 package com.basejava.webapp;
 
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainConcurrency {
-    private final Object firstResource = new Object();
-    private final Object secondResource = new Object();
 
-    public void firstMethod() {
-        synchronized (firstResource) {
-            System.out.println("1st resource has acquired");
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            synchronized (secondResource) {
-                System.out.println("2nd resource has acquired");
-            }
-        }
-    }
+    public static final int NUMBER_OF_THREADS = 10000;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
 
-    public void secondMethod() {
-        synchronized (secondResource) {
-            System.out.println("2nd resource has acquired");
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+    private static void deadLock(Object firstResource, Object secondResource) {
+        new Thread(() -> {
             synchronized (firstResource) {
-                System.out.println("1nd resource has acquired");
+                System.out.println("1st resource has been acquired");
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (secondResource) {
+                    System.out.println("2nd resource has been acquired");
+                }
             }
+        }).start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Object firstResource = new Object();
+        final Object secondResource = new Object();
+
+        deadLock(firstResource, secondResource);
+        deadLock(secondResource, firstResource);
+
+        System.out.println(Thread.currentThread().getName());
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+            }
+
+        };
+        thread0.start();
+
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
+
+        System.out.println(thread0.getState());
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(NUMBER_OF_THREADS);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            Future<Integer> future = executorService.submit(() ->
+            {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                    System.out.println(DATE_FORMAT);
+                }
+                latch.countDown();
+                return 5;
+            });
+            latch.await(10, TimeUnit.SECONDS);
+            executorService.shutdown();
+            System.out.println(mainConcurrency.atomicCounter.get());
         }
     }
 
-    public static void main(String[] args) {
-        MainConcurrency main = new MainConcurrency();
-        Thread firstThread = new Thread(main::firstMethod);
-        Thread secondThread = new Thread(main::secondMethod);
-
-        firstThread.start();
-        secondThread.start();
+    private void inc() {
+        atomicCounter.incrementAndGet();
     }
+
 }
---
+++ src/com/basejava/webapp/MainStream.java
@@ -0,0 +1,32 @@
+package com.basejava.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 2, 3};
+        System.out.println(minValue(arr));
+
+        List<Integer> integers1 = List.of(1, 2, 3);
+        List<Integer> integers2 = List.of(1, 2, 3, 1);
+        System.out.println(oddOrEven(integers1));
+        System.out.println(oddOrEven(integers2));
+    }
+
+    static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .reduce((a, b) -> a * 10 + b)
+                .orElse(0);
+    }
+
+    static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        return integers.stream().filter(n -> (sum % 2 == 0) == (n % 2 != 0))
+                .collect(Collectors.toList());
+    }
+
+}
